674db34ed125aeed2b90ebe869eb28910bd196e4
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-01-21 21:52:41 +0530

    Implemented code highlighting with lexer guessing.

--- rip/workers/code/core.py
+++ rip/workers/code/core.py
@@ -1,4 +1,10 @@
+from docutils import nodes
 from docutils.parsers.rst import directives, Directive
+
+from pygments import highlight
+from pygments.util import ClassNotFound
+from pygments.lexers import get_lexer_by_name, guess_lexer, TextLexer
+from pygments.formatters import HtmlFormatter
 
 from operator import not_
 
@@ -14,6 +20,27 @@ class CodeDirective(Directive) :
     }
 
     has_content = True
+
+    def run(self) :
+        text = u'\n'.join(self.content)
+
+        try :
+            # Use the corresponding lexer, if a language was specified.
+            lexer = get_lexer_by_name(self.arguments[0])
+        except IndexError :
+            try :
+                # No language was specified, so take an educated guess.
+                lexer = guess_lexer(text)
+            except ClassNotFound :
+                # That didn't work either, use a standard TextLexer; no
+                # highlighting.
+                lexer = TextLexer()
+        
+        overrides = self.options
+
+        highlighted_text = highlight(text, lexer, HtmlFormatter(**overrides))
+
+        return [nodes.raw('', highlighted_text, format = 'html')]
 
 def CodeRole() :
     pass
