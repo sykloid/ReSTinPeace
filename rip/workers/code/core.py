6dbc74195c46b5d0b2dbc7284dd98389c515ab94
P.C. Shyamshankar <sykora@lucentbeing.com>
2010-01-21 21:51:01 +0530

    Added code directive option specifications.

--- rip/workers/code/core.py
+++ rip/workers/code/core.py
@@ -1,5 +1,19 @@
+from docutils.parsers.rst import directives, Directive
+
+from operator import not_
+
 class CodeDirective(Directive) :
-    pass
+    '''A ReStructured Text Directive to highlight code blocks using pygments.'''
+
+    require_arguments = 0
+    optional_arguments = 1
+    option_spec = {
+        'linenos' : not_,
+        'style' : directives.unchanged,
+        'noclasses' : not_,
+    }
+
+    has_content = True
 
 def CodeRole() :
     pass
